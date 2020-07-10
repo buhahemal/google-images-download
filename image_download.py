4d2c488d031cfee08bc696364105ef6926989af2
Buha Hemal <40729639+buhahemal@users.noreply.github.com>
2020-07-10 11:08:18 +0530

    Update image_download.py

--- image_download.py
+++ image_download.py
@@ -1,17 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from random import seed
+from random import randint
+import pandas as pd
+excel_file = 'Products.xlsx'
+movies = pd.read_excel(excel_file)
+df = pd.DataFrame(movies)
+for i in df.index:
+    query = df['Product'][i]
+    URL = "https://www.google.co.in/search?q=%s&source=lnms&tbm=isch" % query
+    page = requests.get(URL)
 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    imgclass = soup.find_all("img", {"class": "t0fcAb"})
+    for a in imgclass:
+        print(a['src'])
+        response = requests.get(a['src'])
+        file = open(query+".png", "wb")
+        file.write(response.content)
+        file.close()
+        break 
+    
 
-query = "Dog"
-URL = "https://www.google.co.in/search?q=%s&source=lnms&tbm=isch" % query
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-imgclass = soup.find_all("img", {"class": "t0fcAb"})
-for a in imgclass:
-    print(a['src'])
-    response = requests.get(a['src'])
-    file = open(query+".png", "wb")
-    file.write(response.content)
-    file.close()
-    break #if You Want TO Download Multiplple Just remove Break Statement And Change The Name EVery Time 
